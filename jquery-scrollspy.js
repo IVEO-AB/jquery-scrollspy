75742513eadaae1537417a8be6a6237c65e50477
softwarespot <softwarespotapps@gmail.com>
2015-07-30 08:38:08 +0300

    Fixed JSHinting errors

--- jquery-scrollspy.js
+++ jquery-scrollspy.js
@@ -7,7 +7,7 @@
 
     $.fn.extend({
       scrollspy: function ( options ) {
-        
+
           var defaults = {
             min: 0,
             max: 0,
@@ -18,9 +18,9 @@
             onEnter: options.onEnter ? options.onEnter : [],
             onLeave: options.onLeave ? options.onLeave : [],
             onTick: options.onTick ? options.onTick : []
-          }
-          
-          var options = $.extend( {}, defaults, options );
+          };
+
+          options = $.extend( {}, defaults, options );
 
           return this.each(function (i) {
 
@@ -29,16 +29,17 @@
               var $container = $(o.container);
               var mode = o.mode;
               var buffer = o.buffer;
-              var enters = leaves = 0;
+              var enters = 0;
+              var leaves = 0;
               var inside = false;
-                            
+
               /* add listener to container */
               $container.bind('scroll.' + o.namespace, function(e){
                   var position = {top: $(this).scrollTop(), left: $(this).scrollLeft()};
                   var xy = (mode == 'vertical') ? position.top + buffer : position.left + buffer;
                   var max = o.max;
                   var min = o.min;
-                  
+
                   /* fix max */
                   if($.isFunction(o.max)){
                     max = o.max();
@@ -49,49 +50,49 @@
                     min = o.min();
                   }
 
-                  if(max == 0){
+                  if(max === 0) {
                       max = (mode == 'vertical') ? $container.height() : $container.outerWidth() + $(element).outerWidth();
                   }
-                  
+
                   /* if we have reached the minimum bound but are below the max ... */
                   if(xy >= min && xy <= max){
                     /* trigger enter event */
                     if(!inside){
                        inside = true;
                        enters++;
-                       
+
                        /* fire enter event */
-                       $(element).trigger('scrollEnter', {position: position})
+                       $(element).trigger('scrollEnter', {position: position});
                        if($.isFunction(o.onEnter)){
                          o.onEnter(element, position);
                        }
-                      
+
                      }
-                     
+
                      /* trigger tick event */
-                     $(element).trigger('scrollTick', {position: position, inside: inside, enters: enters, leaves: leaves})
+                     $(element).trigger('scrollTick', {position: position, inside: inside, enters: enters, leaves: leaves});
                      if($.isFunction(o.onTick)){
                        o.onTick(element, position, inside, enters, leaves);
                      }
                   }else{
-                    
+
                     if(inside){
                       inside = false;
                       leaves++;
                       /* trigger leave event */
-                      $(element).trigger('scrollLeave', {position: position, leaves:leaves})
+                      $(element).trigger('scrollLeave', {position: position, leaves:leaves});
 
                       if($.isFunction(o.onLeave)){
                         o.onLeave(element, position);
                       }
                     }
                   }
-              }); 
+              });
 
           });
       }
 
-    })
+    });
 
-    
+
 })( jQuery, window, document, undefined );
