9d223901da2fee5ef90025fbecd3e98ee7dde9ea
softwarespot <softwarespotapps@gmail.com>
2015-08-01 11:23:06 +0300

    Added grunt
    Now use grunt watch to JSHint and minify

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+
+module.exports = function(grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        // watch for changes and trigger compass, jshint, uglify and livereload
+        watch: {
+            js: {
+                files: ['jquery-scrollspy.js'],
+                tasks: ['uglify','jshint'],
+                options: {
+                    livereload: true,
+                },
+            }
+        },
+
+        // uglify to concat & minify
+        uglify: {
+            js: {
+                files: {
+                    'jquery-scrollspy.min.js': 'jquery-scrollspy.js',
+                }
+            }
+        },
+
+        jshint: {
+            all: ['jquery-scrollspy.js']
+        }
+
+    });
+
+    // register task
+    grunt.registerTask('default', ['watch']);
+};
