1dfb0a5677ca05df3d55e4322dc2e5031f2523e4
softwarespot <softwarespotapps@gmail.com>
2015-08-13 19:38:46 +0300

    Updated grunt.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,23 @@
 
 module.exports = function(grunt) {
 
-    // load all grunt tasks
+    // Load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
 
-        // watch for changes and trigger compass, jshint, uglify and livereload
+        // Watch for changes to the main file to trigger uglification and hinting
         watch: {
             js: {
                 files: ['jquery-scrollspy.js'],
-                tasks: ['uglify','jshint'],
+                tasks: ['jshint', 'uglify'],
                 options: {
                     livereload: true,
                 },
             }
         },
 
-        // uglify to concat & minify
-        uglify: {
-            js: {
-                files: {
-                    'jquery-scrollspy.min.js': 'jquery-scrollspy.js',
-                },
-                options: {
-                    compress: {
-                        dead_code: true,
-                        drop_console: true,
-                        unused: true
-                    }
-                }
-            }
-        },
-
+        // Check the code meets the following standards
         jshint: {
             all: ['jquery-scrollspy.js'],
             options: {
@@ -47,10 +32,33 @@ module.exports = function(grunt) {
                   '$': true
                 }
             }
-        }
+        },
+
+        // Uglify aka minify the main file
+        uglify: {
+            js: {
+                files: {
+                    'jquery-scrollspy.min.js': 'jquery-scrollspy.js',
+                },
+                options: {
+                    // See the uglify documentation for more details
+                    compress: {
+                        comparisons: true,
+                        conditionals: true,
+                        dead_code: true,
+                        drop_console: true,
+                        unsafe: true,
+                        unused: true
+                    }
+                }
+            }
+        },
 
     });
 
-    // register task
+    // Register the default task to watch for any changes to the main files
     grunt.registerTask('default', ['watch']);
+
+    // 'grunt jshint' to check the syntax
+    // 'grunt uglify' to uglify the main file
 };
